dc300b3a8e42fa050bb37dcf829b832383334d47
Ayomide Sola-Ayodele <ayomide.sola-ayodele@ucdconnect.ie>
2022-08-30 22:34:55 +0100

    Made class cloneable

--- src/Model/GamePiece.java
+++ src/Model/GamePiece.java
@@ -5,7 +5,8 @@ package Model;
  * @mailto : ayomide.sola-ayodele@ucdconnect.ie
  * @since : 19/07/2022, Tuesday
  **/
-public class GamePiece {
+public class GamePiece implements Cloneable {
+
   enum PieceType {
     PAWN,
     KNIGHT,
@@ -20,7 +21,7 @@ public class GamePiece {
   private char symbol;
   private Location location;
   private boolean wasMoved = false;
-  private boolean lastTurnPassant = false;
+  private boolean movedTwoSpaces = false;
 
   public GamePiece(PieceType pieceType, int playerNo, Location location) {
     this.pieceType = pieceType;
@@ -42,12 +43,25 @@ public class GamePiece {
     this.location = location;
   }
 
+  public GamePiece(PieceType pieceType, int playerNo, char symbol, Location location, boolean wasMoved, boolean movedTwoSpaces) {
+    this.pieceType = pieceType;
+    this.playerNo = playerNo;
+    this.symbol = symbol;
+    this.location = location;
+    this.wasMoved = wasMoved;
+    this.movedTwoSpaces = movedTwoSpaces;
+  }
+
   public char getSymbol() {
     return symbol;
   }
 
   public Location getLocation() {
     return location;
+  }
+
+  public void setLocation(Location location) {
+    this.location = location;
   }
 
   public int getPlayerNo() {
@@ -73,12 +87,17 @@ public class GamePiece {
     this.wasMoved = wasMoved;
   }
 
-  public boolean isLastTurnPassant() {
-    return lastTurnPassant;
+  public boolean movedTwoSpaces() {
+    return movedTwoSpaces;
   }
 
-  public void setLastTurnPassant(boolean lastTurnPassant) {
-    this.lastTurnPassant = lastTurnPassant;
+  public void setMovedTwoSpaces(boolean movedTwoSpaces) {
+    this.movedTwoSpaces = movedTwoSpaces;
+  }
+
+  @Override
+  public GamePiece clone() {
+    return new GamePiece(this.pieceType, this.playerNo, this.symbol, new Location(this.location.getRow(), this.location.getColumn()), this.wasMoved, this.movedTwoSpaces);
   }
 
 }
